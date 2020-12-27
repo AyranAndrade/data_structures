6da332c6d37b20cd76117192b834929d9b61787d
Ayran Andrade <ayran.andrade@guiabolso.com.br>
2020-12-27 16:57:31 -0300

    Add initial structure for hash set

---
+++ hash_set/hash_set.cpp
@@ -0,0 +1,6 @@
+#ifndef _HASH_SET_CPP_
+#define _HASH_SET_CPP_
+
+#include "hash_set.hpp"
+
+#endif
---
+++ hash_set/hash_set.hpp
@@ -0,0 +1,5 @@
+#ifndef _HASH_SET_HPP_
+#define _HASH_SET_HPP_
+
+#include "hash_set.cpp"
+#endif
