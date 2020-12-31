aa2c92592bccfe289ecffed714d23c8d042a37d5
Ayran Andrade <ayran.andrade@guiabolso.com.br>
2020-12-31 13:54:53 -0300

    Add white lines to facilitate reading

--- linked_list/linked_list.hpp
+++ linked_list/linked_list.hpp
@@ -14,20 +14,33 @@ template <class T>
 class LinkedList {
     public:
         void append(T element);
+
         T& get(int index);
+
         int size();
+
         void clear();
+
         bool contains(T element);
+
         int index_of(T element);
+
         int last_index_of(T element);
+
         void remove(int index);
+
         void insert(T element, int index);
+
         LinkedList sub_list(int from, int to);
+
         T& operator[](int index);
+
         LinkedList();
     private:
         LinkedList(Node<T> head, int length);
+
         Node<T> head;
+
         int length = 0;
 };
 
--- list/list.hpp
+++ list/list.hpp
@@ -5,23 +5,39 @@ template<class T>
 class List {
     public:
         void append(T element);
+
         T& get(int index);
+
         int size();
+
         void clear();
+
         bool contains(T element);
+
         int index_of(T element);
+
         int last_index_of(T element);
+
         void remove(int index);
+
         void insert(T element, int index);
+
         List sub_list(int from, int to);
+
         T& operator[](int index);
+
         List();
     private:
         List(T *array, int max_length, int length);
+
         void resize_if_necessary();
+
         T *array = new T[10];
+
         int INITIAL_MAX_LENGTH = 10;
+
         int max_length = INITIAL_MAX_LENGTH;
+
         int length = 0;
 };
 
