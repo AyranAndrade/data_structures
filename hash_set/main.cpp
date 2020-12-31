90783731a2257be50b9170f0348925dbbe9ccf81
Ayran Andrade <ayran.andrade@guiabolso.com.br>
2020-12-31 17:43:00 -0300

    Remove commented code on hash set

--- hash_set/main.cpp
+++ hash_set/main.cpp
@@ -6,45 +6,6 @@
 using namespace std;
 
 int main() {
-    // List<LinkedList<int>> test;
-    // test.append(LinkedList<int>());
-    // test.get(0).insert(3, 0);
-    // test.get(0).insert(2, 0);
-    // test.get(0).insert(1, 0);
-
-    // cout << "test1: " << test.get(0) << endl;
-
-    // LinkedList<int> test2 = test.get(0);
-    // test2.insert(0,0);
-    // test.get(0).insert(-1,0);
-
-    // cout << "test2: " << test2 << endl;
-    // cout << "test1: " << test.get(0) << endl;
-    // cout << "test2: " << &test2 << endl;
-    // cout << "test1: " << &test.get(0) << endl;
-    // cout << "--------------------------------------------" << endl;
-
-    // vector<vector<int>> g1;
-    // g1.push_back(vector<int>());
-    // vector<int> g2 = g1[0];
-    // g2.push_back(1);
-    // g2.push_back(2);
-    // g2.push_back(3);
-
-    // cout << g2[0] << endl;
-    // cout << g2[1] << endl;
-    // cout << g2[2] << endl;
-    // cout << g2.size() << endl;
-
-    // vector<int> g3 = g1[0];
-    // cout << "here" << endl;
-    // cout << g3.size() << endl;
-    // cout << g3[1] << endl;
-    // cout << g3[2] << endl;
-
-    // cout << "--------------------------------------------" << endl;
-
-
     HashSet set;
 
     set.add(1);
